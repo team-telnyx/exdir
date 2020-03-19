96113bc1ebe6327eed1c1a71b56359cc3c017cdd
Guilherme Balena Versiani <guilherme@telnyx.com>
2020-03-19 08:35:07 -0300

    Typo.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExDir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
       package: package(),
       name: "ExDir",
       source_url: "https://github.com/team-telnyx/exdir",
